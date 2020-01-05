de29e73f89d76c940dc8c4206a4a0fab2388a223
brian <he201459@students.ephec.be>
2020-01-05 20:18:30 +0100

    Initial commit

---
+++ include/header.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/fontawesome.min.css"
+          integrity="sha256-/sdxenK1NDowSNuphgwjv8wSosSNZB0t5koXqd7XqOI=" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/solid.min.css"
+          integrity="sha256-8DcgqUGhWHHsTLj1qcGr0OuPbKkN1RwDjIbZ6DKh/RA=" crossorigin="anonymous"/>
+    <!-- Base CSS -->
+    <link rel="stylesheet" href="css/base.css">
+
+    <title><?= $title ?></title>
+</head>
+<body>
+
+<header class="navbar navbar-expand-lg navbar-dark main-navbar">
+    <div class="container">
+        <a class="navbar-brand mr-auto" href="index.html">RixRefugees</a>
+        <span class="user-name">Prénom Nom</span>
+    </div>
+
+</header>
+
+<nav class="navbar navbar-expand-lg navbar-light sub-navbar">
+    <div class="container">
+        <ul class="nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><i class="fas fa-home"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Réfugier</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Logement</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Bénévole</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Niveau stock</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Coordinateur</a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+$title = "RixRefugiees";
+include_once "../include/header.php"
+
+?>
+
+
+<?php
+include_once "../include/footer.php"
+?>
